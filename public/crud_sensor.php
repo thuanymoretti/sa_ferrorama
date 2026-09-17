e47ab177ef985ae0ad49f064acc29aea2cbcf288
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-09-17 10:13:51 -0300

    add - crud do sensor

--- public/crud_sensor.php
+++ public/crud_sensor.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once "../infra/conexao.php";
 
 
@@ -6,16 +7,22 @@ $sql = "SELECT * FROM sensores";
 $stmt = $conexao->query($sql);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
+
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Sensores cadastrados</title>
 
     <link rel="stylesheet" href="../assets/style/style.css">
+
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
 
@@ -24,53 +31,86 @@ $stmt = $conexao->query($sql);
 
     <header class="cabecalho">
 
-        <h2>   Bem vindo!!  </h2>
-<div class="layout">
-            <aside class="menu-lateral">
-                <a href="home.php" class="item ativo"><i class="bi bi-grid-1x2-fill"></i> Dashboard</a>
-                <a href="cadastrar_sensor.html" class="item"><i class="bi bi-cpu-fill"></i> Sensores</a>
-                <a href="crud_trens.php" class="item"><i class="bi bi-file-earmark-bar-graph-fill"></i> Trens</a>
-                <a href="crud_usuarios.php" class="item"><i class="bi bi-people-fill"></i> Cadastrados</a>
-                <a href="crud_viagens.php" class="item"><i class="bi bi-gear-fill"></i> Viagens</a>
-            </aside>
+        <h2>  Bem-vind! </h2>
+
     </header>
+
+
+    <div class="layout">
+
+
+        <aside class="menu-lateral">
+
+            <a href="home.php" class="item">  Dashboard  </a>
+
+            <a href="crud_sensor.php" class="item ativo"> Sensores  </a>
+
+            <a href="crud_trens.php" class="item">  Trens </a>
+
+            <a href="crud_usuarios.php" class="item">   Cadastrados </a>
+
+            <a href="crud_viagens.php" class="item">  Viagens </a>
+
+        </aside>
+
+
+        <main class="conteudo">
+
+
+            <div class="titulo_sensores">
+                <div>
+                    <h1 > Sensores cadastrados  </h1>              
+                </div>
+            </div>
+
+
+            <div class="planilha_dashboard">
+
  <table class="table table-borderless">
 
-                    <thead>
+ <thead>
+ <tr>
+                            <th>ID</th> 
+                             <th>NOME</th>
+                             <th>IDENTIFICAÇÃO</th>
+                             <th>LOCALIZAÇÃO</th>
+                             <th>TIPO DE DADO</th>
+                             <th>TREM VINCULADO</th>
+                             <th>AÇÕES</th>
 
-                        <tr>
-                            <th>NOME</th>
-                            <th>LOCALIZAÇÃO</th>
-                            <th>TIPO DE DADO</th>
-                            <th>TREM VINCULADO</th>
-                            <th>AÇÕES</th>
-        
                         </tr>
+
                     </thead>
 
-                    <tbody>
-                        <div>
+
+                    <body>
+
+
                         <?php while ($sensor = $stmt->fetch_assoc()) { ?>
 
+
                             <tr>
-                                <td> <?php echo $sensor['nome']; ?></td>
-                                <td> <?php echo $sensor['localizacao']; ?> </td>
+
+                                <td> #<?php echo $sensor['id']; ?>  </td>
+                                <td> <?php echo $sensor['nome']; ?> </td>
+                                <td> <?php echo $sensor['identificacao']; ?> </td>
+                                <td> <?php echo $sensor['localizacao']; ?>  </td>
                                 <td> <?php echo $sensor['tipo_sensor']; ?> </td>
-                                <td> <?php echo $sensor['trem_id']; ?> </td>
-                            </td>
+                                <td> <?php echo $sensor['trem_id']; ?>  </td>
 
-                                <td>
+
+                               <td>
                                     <a href="editar_sensor.php?id=<?php echo $sensor['id']; ?>"> Editar </a>
-
                                     <a href="excluir_sensor.php?id=<?php echo $sensor['id']; ?>"> Excluir </a>
                                 </td>
                             </tr>
+                        <?php } ?>
+                    </body>
+                </table>
+                 <a href="cadastrar_sensor.php"> <button type="button">Cadastrar novo sensor </button> </a>
+            </div>
 
-                            
-                            <?php } ?>
-                            <br>
-                            <br>
-                           <a href="cadastrar_sensor.php" class="btn btn-primery"> Cadastrar novo sensor</a>
-
-                    </div>
-0
+        </main>
+    </div>
+</body>
+</html>
